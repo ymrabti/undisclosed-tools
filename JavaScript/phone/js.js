5af37b6dc55d586714e62226cf08e3b451ef7f42
ymrabti <47449165+ymrabti@users.noreply.github.com>
2021-02-23 22:17:22 +0100

    new changes1

--- JavaScript/phone/js.js
+++
@@ -1,3 +0,0 @@
-var text="200 dh tazmoutine.5  dh grand taxi.6 dh tram.12 dh tram.95 dh librairie.22 dh food.6 dh tram.6 dh tram.15 dh grand taxi.7,5 grand taxi.7,5 grand taxi.23 grand taxi.12 tram.10,5 food.23 grand taxi.7,5 grand taxi.13,5 librairie.20 dh librairie.20 dh arrondissement.15 dh grand taxi.9 dh librairie.34 dh food.10 dh librairie.7,5 dh grand taxi.3,5 dh food.23 dh grand taxi.12 dh tram.7,5 dh grand taxi.33 dh tran.4 dh pisri.15 dh grand taxi.5 dh pisri.20 dh tobis.6 dh grand taxi.45 dh grand taxi.7,5 dh grand taxi.4 dh librairie.7,5 dh grand taxi.120 dh tran";
-var list1='"'+text.split(".").join('."\n+"')+'"';
-console.log(list1);copy(list1);
--- JavaScript/phone/scriptjs.js
+++
@@ -1,4 +0,0 @@
-var text="7,5 dh grand taxi.7 dh petit taxi.21 dh grand taxi.7 dh grand taxi.25 dh triporteur.200 dh tazmoutine.10 dh sndou9";
-var list1=text.split(".");
-console.log(list1);
-
